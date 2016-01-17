b8705a9463ecadb329e0f3be0fc61fe9752f9bf3
BrunoMNDantas <bmndantas@gmail.com>
2016-01-17 04:08:42 +0000

    ChainsFrom and ChainsTo removed from order view

--- NGSPipesEditor/src/editor/userInterface/controllers/FXMLStepsOrderController.java
+++ NGSPipesEditor/src/editor/userInterface/controllers/FXMLStepsOrderController.java
@@ -12,9 +12,7 @@ import components.FXMLFile;
 
 import editor.dataAccess.Uris;
 import editor.dataAccess.loader.LogoLoader;
-import editor.logic.entities.EditorChain;
 import editor.logic.entities.EditorStep;
-import editor.logic.entities.Elements;
 
 public class FXMLStepsOrderController implements IInitializable<EditorStep>{
 	
@@ -36,10 +34,6 @@ public class FXMLStepsOrderController implements IInitializable<EditorStep>{
 	@FXML
 	private Label lCommand;
 	@FXML
-	private Label lChainsFrom;
-	@FXML
-	private Label lChainsTo;
-	@FXML
 	private ImageView iVLogo;
 	
 	private EditorStep step;
@@ -54,41 +48,15 @@ public class FXMLStepsOrderController implements IInitializable<EditorStep>{
 		setOrder(step.getOrder());
 		lTool.setText(step.getToolDescriptor().getName());
 		lCommand.setText(step.getCommandDescriptor().getName());
-		setChainsFrom();
-		setChainsTo();
 		
 		iVLogo.setImage(DEFAULT_TOOL_LOGO);
 		new LogoLoader(step.getToolDescriptor(), iVLogo).load();
 		
 		step.orderEvent.addListner(this::setOrder);
-		Elements elements = step.getOriginFlow().getElements();
-		elements.addChainEvent.addListner(this::setChains);
-		elements.removeChainEvent.addListner(this::setChains);
 	}
 	
 	private void setOrder(int order){
 		lOrder.setText(order + "º");
 	}
-	
-	private void setChains(EditorChain chain){
-		if(chain.getFrom() == step)
-			setChainsFrom();
-		if(chain.getTo() == step)
-			setChainsTo();
-	}
-	
-	private void setChainsFrom(){
-		if(step.getOriginFlow() == null)
-			lChainsFrom.setText("");
-		else
-			lChainsFrom.setText(step.getOriginFlow().getElements().getChainsFrom(step).size() + "");
-	}
-	
-	private void setChainsTo(){
-		if(step.getOriginFlow() == null)
-			lChainsTo.setText("");
-		else
-			lChainsTo.setText(step.getOriginFlow().getElements().getChainsTo(step).size() + "");
-	}
-	
+		
 }
